d84a4210cc44e60b8430d32f817d7160355a245f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-04-24 14:04:37 +0300

    Add project files.

---
+++ Persistence/Contexts/RentACarDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class RentACarDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=Tobeto4A.Onion; Trusted_Connection=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,29 @@
+using Persistence;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddPersistenceServices();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+// 2:20
